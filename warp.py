ac0916f713c8aca07c6013559c64888f8b622e7e
4-0-4 <146018190+nyeinkokoaung404@users.noreply.github.com>
2024-10-04 02:41:39 +0630

    Update warp.py

--- warp.py
+++ warp.py
@@ -175,7 +175,7 @@ def export_SingBox(t_ips):
     with open("assets/singbox-template.json", "r") as f:
         data = json.load(f)
 
-    data["outbounds"][3]["outbounds"].extend(["🚀TEHRAN-404", "🚀BERLIN-404"])
+    data["outbounds"][1]["outbounds"].extend(["🚀TEHRAN-404", "🚀BERLIN-404"])
 
     tehran_wg = toSingBox("🚀TEHRAN-404", t_ips[0], "direct")
     if tehran_wg:
