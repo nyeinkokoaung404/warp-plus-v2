944a8cd1c2aca33972e4454dfd21794507d886ba
4-0-4 <146018190+nyeinkokoaung404@users.noreply.github.com>
2024-10-05 01:50:33 +0630

    Update warp.py

--- warp.py
+++ warp.py
@@ -206,6 +206,18 @@ def export_SingBox(t_ips):
     else:
         print("Failed to generate 🚀BERLIN-404 configuration")
 
+    tehran_nkka = toSingBox("TEHRAN", t_ips[0], "direct")
+    if tehran_nkka:
+        data["outbounds"].insert(2, tehran_nkka)
+    else:
+        print("Failed to generate TEHRAN configuration")
+
+    berlin_nkka = toSingBox("BERLIN", t_ips[1], "TEHRAN")
+    if berlin_nkka:
+        data["outbounds"].insert(3, berlin_nkka)
+    else:
+        print("Failed to generate BERLIN configuration")
+
     with open("sing-box.json", "w") as f:
         json.dump(data, f, indent=4)
 
